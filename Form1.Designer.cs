0df268d8542d123dde12c00da2d7b83ae9496672
Dovzenok Dima <dima.dovzenok2003@gmail.com>
2020-05-19 15:09:40 +0300

    Add project files

---
+++ Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.button1.FlatAppearance.BorderSize = 10;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Mongolian Baiti", 27.75F);
+            this.button1.Location = new System.Drawing.Point(343, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(167, 108);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Запуск";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 361);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Работа с треугольком";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Triangle.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class Triangle
+    {
+        public double a; // первая сторона 
+        public double b; // вторая сторона
+        public double c; // третья сторона
+        public Triangle (double A, double B, double C)
+        {
+            a = A;
+            b = B;
+            c = C;
+        }
+        public string outputA() // выводим сторону а, данный метод возвращает строковое значение
+        {
+            return Convert.ToString(a); // a - ссылка на внутренее поле обьекта класса
+        }
+        public string outputB() 
+        {
+                return Convert.ToString(b);
+        }
+        public string outputC()
+        {
+            return Convert.ToString(c);
+        }
+        public double Perimeter() // сумма всех сторон типо double
+        {
+            double p = 0;
+            p = a + b + c; // вычисление
+            return p; // возврат
+        }
+        public double Surface() // аналогично периметру
+        {
+            double s = 0;
+            double p = 0;
+            p = (a + b + c) / 2;
+            s = Math.Sqrt((p * (p - a) * (p - b) * (p - c)));
+            return s;
+        }
+        public double GetSetA // свойство позволяющее установить либо изменить значение стороны а
+        {
+            get //устанавливаем
+            { return a; }
+            set // меняем
+            { a = value;}
+        }
+        public double GetSetB
+        {
+            get
+            { return b; }
+            set
+            { b = value; }
+        }
+        public double GetSetC
+        {
+            get
+            { return c; }
+            set
+            { c = value; }
+        }
+        public bool ExistTriangle // свойство позволяющее установить, существует ои треугольник с задаными сторонами
+        {
+            get
+            {
+                if ((a > b + c) && (b > a + c) && (c > a + b)) //сумма 2 сторон должна быть больше третьей
+                return false;
+                else return true;
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
