999f3674913f162b1e1623fa95dc2b4522f5a909
Dovzenok Dima <dima.dovzenok2003@gmail.com>
2020-05-27 11:14:22 +0300

    run_button

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,55 +29,100 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.button1 = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.Run_button = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.txtC = new System.Windows.Forms.TextBox();
+            this.txtA = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // button1
+            // Run_button
             // 
-            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
-            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
-            this.button1.FlatAppearance.BorderSize = 10;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Font = new System.Drawing.Font("Mongolian Baiti", 27.75F);
-            this.button1.Location = new System.Drawing.Point(620, -2);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(167, 108);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Запуск";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.Run_button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.Run_button.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.Run_button.FlatAppearance.BorderSize = 10;
+            this.Run_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Run_button.Font = new System.Drawing.Font("Mongolian Baiti", 27.75F);
+            this.Run_button.Location = new System.Drawing.Point(710, 0);
+            this.Run_button.Name = "Run_button";
+            this.Run_button.Size = new System.Drawing.Size(167, 108);
+            this.Run_button.TabIndex = 0;
+            this.Run_button.Text = "Запуск";
+            this.Run_button.UseVisualStyleBackColor = false;
+            this.Run_button.Click += new System.EventHandler(this.Run_button_Click);
             // 
-            // pictureBox1
+            // listView1
             // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(453, 135);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(334, 228);
-            this.pictureBox1.TabIndex = 1;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(83, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(252, 158);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Поле";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Значение";
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(83, 214);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(111, 20);
+            this.txtB.TabIndex = 2;
+            // 
+            // txtC
+            // 
+            this.txtC.Location = new System.Drawing.Point(83, 240);
+            this.txtC.Name = "txtC";
+            this.txtC.Size = new System.Drawing.Size(111, 20);
+            this.txtC.TabIndex = 3;
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(83, 188);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(111, 20);
+            this.txtA.TabIndex = 4;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(784, 361);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(877, 361);
+            this.Controls.Add(this.txtA);
+            this.Controls.Add(this.txtC);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.Run_button);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Работа с треугольком";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.Text = "Работа с треугольником";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button Run_button;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.TextBox txtC;
+        private System.Windows.Forms.TextBox txtA;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,15 +16,46 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
-
-        private void button1_Click(object sender, EventArgs e)
+        private void Form1_Load(object sender, EventArgs e)
         {
-
+            
         }
-
-        private void pictureBox1_Click(object sender, EventArgs e)
+        private void Run_button_Click(object sender, EventArgs e)
         {
+            double a, b, c;
+            a = Convert.ToDouble(txtA.Text);
+            b = Convert.ToDouble(txtB.Text);
+            c = Convert.ToDouble(txtC.Text);
 
+            Triangle triangle = new Triangle(a, b, c);
+
+            listView1.Items.Add("Сторона а");
+            listView1.Items.Add("Сторона b");
+            listView1.Items.Add("Сторона c");
+
+            listView1.Items.Add("Периметр");
+            listView1.Items.Add("Площадь");
+            listView1.Items.Add("Полупериметр");
+
+            listView1.Items.Add("Существует?");
+            listView1.Items.Add("Спецификатор");
+
+            listView1.Items[0].SubItems.Add(triangle.outputA());
+            listView1.Items[1].SubItems.Add(triangle.outputB());
+            listView1.Items[2].SubItems.Add(triangle.outputC());
+            listView1.Items[3].SubItems.Add(Convert.ToString(triangle.Perimeter()));
+            listView1.Items[4].SubItems.Add(Convert.ToString(triangle.Surface()));
+            listView1.Items[5].SubItems.Add(Convert.ToString(triangle.SemiPerimeter()));
+            if (triangle.ExistTriangle) { listView1.Items[6].SubItems.Add("Существует"); }
+            else listView1.Items[6].SubItems.Add("Не существует");
         }
     }
 }
+
+
+
+
+
+
+
+
